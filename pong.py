f066f3ce81e0354f7bd9766ee54d543e177ed395
ajay-nagpal <ajaynagpal.an64@gmail.com>
2026-04-15 04:25:43 +0530

    player paddle movement

--- pong.py
+++ pong.py
@@ -18,18 +18,20 @@ light_grey=(200,200,200)
 ball = pygame.Rect(0, 0, 30, 30)
 ball.center =(screen_width//2,screen_height//2)
 
-cpu = pygame.Rect(0, 0, 20, 100)
-cpu.centery =screen_height//2
+cpu_paddle = pygame.Rect(0, 0, 20, 100)
+cpu_paddle.centery =screen_height//2
 
-player = pygame.Rect(0, 0, 20, 100)
-player.midright =(screen_width,screen_height//2)
+player_paddle = pygame.Rect(0, 0, 20, 100)
+player_paddle.midright =(screen_width,screen_height//2)
 
 ball_speed_x=6
 ball_speed_y=6
 
+player_paddle_speed=0
+
 def animate_ball():
     global ball_speed_x,ball_speed_y
-    
+
     ball.x+=ball_speed_x
     ball.y+=ball_speed_y
 
@@ -50,26 +52,37 @@ while not exit_game:
             break
 
     #2 update position
-
+        #key press
         if event.type==pygame.KEYDOWN:
-            if event.key==pygame.K_LEFT:
-                pass
-            elif event.key==pygame.K_RIGHT:
-                pass
-            elif event.key==pygame.K_DOWN:
-                pass
+            #move paddle
+            if event.key==pygame.K_DOWN:
+                player_paddle_speed=5
             elif event.key==pygame.K_UP:
-                pass
+                player_paddle_speed=-5
+        
+        #key release
+        if event.type==pygame.KEYUP:
+            #move paddle
+            if event.key==pygame.K_DOWN:
+                player_paddle_speed=0
+            elif event.key==pygame.K_UP:
+                player_paddle_speed=0
     
     animate_ball()
+    player_paddle.y+=player_paddle_speed
+
+    if player_paddle.top<=0:
+        player_paddle.top=0
+    if player_paddle.bottom>=screen_height:
+        player_paddle.bottom=screen_height
         
     #3 drawing
     #this willl stop traces of previous draw ball
     screen.fill(dark_grey_blue)
     
     pygame.draw.ellipse(screen,light_grey,ball)
-    pygame.draw.rect(screen,light_grey,cpu)
-    pygame.draw.rect(screen,light_grey,player)
+    pygame.draw.rect(screen,light_grey,cpu_paddle)
+    pygame.draw.rect(screen,light_grey,player_paddle)
 
     pygame.draw.aaline(screen,light_grey,(screen_width//2,0),(screen_width//2,screen_height))
     #update the display
